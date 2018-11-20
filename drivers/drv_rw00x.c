39bf6873d73de03495b940c5fe24c37e01c0b079
zyh <lymz@foxmail.com>
2018-11-20 17:46:11 +0800

    更新rw00x驱动 提高传输效率和传输的正确率

--- drivers/drv_rw00x.c
+++ drivers/drv_rw00x.c
@@ -1,11 +1,12 @@
 #include <rtthread.h>
 #include <rtdevice.h>
 #include <drv_spi.h>
+#include <board.h>
 #define SPI_BUS_NAME    "spi2"
-#define CS_PIN          102 //PH6
-#define INT_BUSY_PIN    62  //PA7
-#define RESET_PIN       187 //PG10
-#define MODE_BIT0_PIN   175 //PD3
+#define CS_PIN          D10 //PH6
+#define INT_BUSY_PIN    D9  //PA7
+#define RESET_PIN       D8  //PG10
+#define MODE_BIT0_PIN   D13 //PD3
 #define MODE_BIT1_PIN   CS_PIN
 #define MODE_READY_PIN  INT_BUSY_PIN
 extern void spi_wifi_isr(int vector);
@@ -31,10 +32,18 @@ void wifi_spi_device_init(const char * device_name)
     set_rw00x_mode(1);
     stm32_spi_bus_attach_device(CS_PIN, SPI_BUS_NAME, device_name);
 }
+static void int_wifi_irq(void * p)
+{
+    ((void)p);
+    if(rt_pin_read(INT_BUSY_PIN))
+    {
+        spi_wifi_isr(0);
+    }
+}
 
 void spi_wifi_hw_init(void)
 {
-    rt_pin_attach_irq(INT_BUSY_PIN, PIN_IRQ_MODE_FALLING, (void(*)(void *))spi_wifi_isr, 0);
+    rt_pin_attach_irq(INT_BUSY_PIN, PIN_IRQ_MODE_FALLING, int_wifi_irq, 0);
     rt_pin_irq_enable(INT_BUSY_PIN, RT_TRUE);
 }
 
@@ -45,7 +54,10 @@ rt_bool_t spi_wifi_is_busy(void)
 
 void spi_wifi_int_cmd(rt_bool_t cmd)
 {
+    rt_pin_write(D6, cmd);
+    rt_pin_write(D5, rt_pin_read(INT_BUSY_PIN));
     rt_pin_irq_enable(INT_BUSY_PIN, cmd);
+    rt_pin_write(D5, rt_pin_read(INT_BUSY_PIN));
 }
 
 
